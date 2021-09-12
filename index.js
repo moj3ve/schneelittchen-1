1839bbaf99246b4ea1d4ba24b45c9b24a0bb358f
alexandra <83172201+schneelittchen@users.noreply.github.com>
2021-09-12 22:04:25 +0200

    added new beta packages

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", function() {
+    document.getElementById('background').style.backgroundPositionY = window.pageYOffset * 0.7 + "px";
+})
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        let offset = $(window).scrollTop();
+        $('.background').css("opacity", 0 + offset / 500);
+        $('.header').css("opacity", 1 - offset / 300);
+        $('.tweaks').css("opacity", 0 + offset / 300);
+        
+        if (offset > 0) $('.scrollIndicator').css("visibility", "hidden");
+    })
+})
